522a99766bf8b5b6ed3408b3ce915d0bbdb82189
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-04-24 10:02:56 -0700

    changes made

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,8 @@
 import React from "react";
 import axios from 'axios'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  
-  const state = {
+class Login extends React.Component{
+ state = {
     credentials: {
       username: '',
       password: ''
@@ -14,7 +11,7 @@ const Login = () => {
 
 
 
-  const handleChange = e => {
+ handleChange = e => {
     this.setState({
       credentials: {
         ...this.state.credentials,
@@ -23,30 +20,42 @@ const Login = () => {
     });
   };
 
+login = e => {
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route'
+  console.log(this.state.credentials)
+  e.preventDefault()
   axios
-    .post(`http://localhost:5000/api/login`, state.credentials)
+    .post('http://localhost:3000/api/login', this.state.credentials)
+    .then(res => console.log(res))
+     .catch(err => console.log({err}))
 
 
+}
+
+  render(){
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-      <form>
+      <form onSubmit={this.login}>
           <input
             type="text"
             name="username"
-            value={state.credentials.username}
-            onChange={handleChange}
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
           />
           <input
             type="password"
             name="password"
-            value={state.credentials.password}
-            onChange={handleChange}
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
           />
+          <button>Sign-in</button>
        </form>
     </>
   );
 };
+}
 
 export default Login;
